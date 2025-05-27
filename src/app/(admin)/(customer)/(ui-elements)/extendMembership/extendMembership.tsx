e28f40a4a20e6db6074b12fae424af828efd3ed4
tomy21 <tomy21.agung@gmail.com>
2025-05-27 10:00:37 +0700

    updated

--- src/app/(admin)/(customer)/(ui-elements)/extendMembership/extendMembership.tsx
+++ src/app/(admin)/(customer)/(ui-elements)/extendMembership/extendMembership.tsx
@@ -184,12 +184,12 @@ export default function ExtendMembership() {
         <div className="flex w-full items-center justify-between">
           <span
             className={`rounded-lg px-3 py-3 text-xs font-semibold ${
-              isActive
+              isExpired
                 ? "bg-green-100 text-green-700"
                 : "bg-red-100 text-red-600"
             }`}
           >
-            {isActive ? "Active" : "Expired"}
+            {isExpired ? "Active" : "Expired"}
           </span>
           {!isActive ||
             (!isExpired && (
