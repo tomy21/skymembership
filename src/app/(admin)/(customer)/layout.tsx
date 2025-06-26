47f5252fe7a5f9200da9a0d2b5b4598d5c716bed
tomy21 <tomy21.agung@gmail.com>
2025-06-26 11:29:15 +0700

    updated

--- src/app/(admin)/(customer)/layout.tsx
+++ src/app/(admin)/(customer)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import ProtectedLayout from "@/app/(protected)/layout";
+import DevToolsWarning from "@/components/DevToolsWarning";
 import ProgressBarProvider from "@/components/ProgressBarProvider";
 import React from "react";
 import { Toaster } from "sonner";
@@ -12,6 +13,7 @@ export default function CustomerLayout({
   return (
     <div className="m-auto flex min-h-screen w-full items-center justify-center bg-white sm:container sm:w-full md:container md:w-1/4 xl:flex">
       <ProtectedLayout>
+        <DevToolsWarning />
         <ProgressBarProvider>{children}</ProgressBarProvider>
         <Toaster richColors position="top-right" duration={1000} />
       </ProtectedLayout>
