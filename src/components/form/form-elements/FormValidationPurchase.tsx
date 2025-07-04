3b27298d4e9e15551fea391a6bc72225f89c705b
tomy21 <tomy21.agung@gmail.com>
2025-07-04 09:40:23 +0700

    updated

--- src/components/form/form-elements/FormValidationPurchase.tsx
+++ src/components/form/form-elements/FormValidationPurchase.tsx
@@ -161,7 +161,7 @@ export default function ConfirmationForm() {
 
   return (
     <>
-      <div className="min-h-screen">
+      <div className="min-h-screen p-5">
         <div className="mx-auto max-w-full py-6">
           <div className="flex w-full flex-col items-center justify-between space-y-3">
             <div className="w-full space-y-3 rounded-xl border border-gray-200 bg-gray-50 p-5 shadow-sm">
